72d95ab46c479945c5f8f5be838d642102bd864a
bsandoval <bsandoval@sodep.com.py>
2017-07-27 16:39:30 -0400

    Async behavior answers

--- app/async.js
+++ app/async.js
@@ -2,10 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise((resolve) => {
+        resolve(value);
+    });
   },
 
   manipulateRemoteData: function(url) {
-
+    return new Promise((resolve, reject) => {
+      $.ajax({
+          type: 'GET',
+          url: url,
+          success: function(data) {
+            var array = [];
+            for (i = 0; i < data.people.length; i++) {
+              array.push(data.people[i].name);
+            }
+            resolve(array.sort());
+          }
+      }).fail(function (error) {
+          reject(error);
+      });
+    });
   }
 };
